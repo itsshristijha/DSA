e00ec0119146b6613415582513d343dc6060b0c4
Shristi Jha <136948383+itsshristijha@users.noreply.github.com>
2026-05-14 23:12:37 +0530

    Create Palindromenumber.cpp

---
+++ Palindromenumber.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i =0;i<n;i++){
+        cin >> arr[i];
+    }
+    bool flag = true;
+    for(int i = 0;i <=n/2;i++){
+        if(arr[i] != arr[n-i-1]){
+            flag = false;
+        }
+    }
+    if(flag == true){
+        cout << "palindrome";
+    }else{
+        cout << "not palindrome";
+    }
+    return 0;
+}
